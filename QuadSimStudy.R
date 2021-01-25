81992a6ff314fc331f31fcf62c1f12a2477b5a73
parqueted <j.murray7@ncl.ac.uk>
2021-01-25 10:21:13 +0000

    Re-specified Sigma

--- QuadSimStudy.R
+++ QuadSimStudy.R
@@ -19,9 +19,9 @@ library(survival)
 source("~/Documents/PhD/SMMR_Simulations/simdata.R")
 
 # Set out covariance matrix -----
-sigma.0 <- 1   # Intercept RE
+sigma.0 <- 1     # Intercept RE
 sigma.1 <- 0.5   # Slope RE
-sigma.2 <- 0.15   # Quadratic RE 
+sigma.2 <- 0.15  # Quadratic RE 
 
 Sigma <- matrix(
   c(sigma.0 ^ 2,       sigma.0 * sigma.1, sigma.0 * sigma.2,
